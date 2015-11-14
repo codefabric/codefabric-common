fbbd1f2292cef60324d31239e678283a2b973b63
David Else <david@codefabric.co.uk>
2015-11-14 18:43:27 +0000

    Adding git repo path and summary to package config

--- package.js
+++ package.js
@@ -2,7 +2,8 @@ Package.describe({
   name: 'codefabric:meteor-common',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Useful utilities and base classes for writing Meteor packages',
+  git: 'https://github.com/codefabric/codefabric-meteor-common.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
